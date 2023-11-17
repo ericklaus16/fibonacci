5dec630a6c765bd8576d7508c808ca7f2cfa083e
Eric Klaus <ericklaus16@gmail.com>
2023-11-17 17:15:33 -0300

    Comentários para facilitar o entendimento

--- fibo.asm
+++ fibo.asm
@@ -3,7 +3,7 @@ section .data
     erro : db "Algo deu errado.", 10, 0
     erroL: equ $ - erro
     inicio_arquivo: db "fib(", 0
-    fim_arquivo: db ").bin", 0 ; Os dois se juntaram para formar o nome do arquivo
+    fim_arquivo: db ").bin", 0 ; Os dois se juntarão para formar o nome do arquivo
 
 section .bss
     input: resb 3 ; 2 digitos + espaco
@@ -18,26 +18,26 @@ _start:
     mov rax, 0
     mov rdi, 0
     lea rsi, [input]
-	mov rdx, 3
-    syscall
+    mov rdx, 3
+    syscall ; nesimo lido
 
-    cmp byte[input], '-'
+    cmp byte[input], '-' ; O número é negativo?
     je errorMsg
 
-    cmp byte[input + 1], 10
+    cmp byte[input + 1], 10 ; Foi lido apenas um numero?
     je oneNumber
 
-    cmp byte[input + 2], 10
+    cmp byte[input + 2], 10 ; 2 números?
     je twoNumbers
-    jne handle
+    jne handle ; São 3 números ou mais...
 
     oneNumber:
         mov al, [input]
         mov rbx, [inicio_arquivo]
         mov [f_arquivo], rbx ; fib(
-        mov [f_arquivo + 4], al ;fib(x
+        mov [f_arquivo + 4], al ; fib(x
         mov rbx, [fim_arquivo]
-        mov [f_arquivo + 5], rbx; fib(x)
+        mov [f_arquivo + 5], rbx ; fib(x)
         mov bl, [fim_arquivo + 4]
         mov [f_arquivo + 9], bl
         sub al, 48
@@ -54,32 +54,32 @@ _start:
     twoNumbers:
         mov cl, [input + 1]
         mov al, [input]
-        mov rbx, [inicio_arquivo]
+        mov rbx, [inicio_arquivo] ; fib(
         mov [f_arquivo], rbx
-        mov [f_arquivo + 4], al
-        mov [f_arquivo + 5], cl
-        mov rbx, [fim_arquivo]
+        mov [f_arquivo + 4], al ; fib(x
+        mov [f_arquivo + 5], cl ; fib(xy
+        mov rbx, [fim_arquivo] ; fib(xy).bin
         mov [f_arquivo + 6], rbx
         mov bl, [fim_arquivo + 4]
         mov [f_arquivo + 10], bl
-        sub al, 48
-        sub cl, 48
+        sub al, 48 ; Convertendo o valor lido em string para numérico
+        sub cl, 48 ; Convertendo o valor lido em string para numérico
         imul ax, 10
-        add al, cl
-        mov [fib_arq], al
+        add al, cl ; al = cl
+        mov [fib_arq], al ; al = fib_arq
         cmp al, 94
         jge errorMsg
         mov r15, 1
 
     fibonacci:
-        mov r13, r15
-        add r15, r14
-        mov [fib], r15
-        mov r14, r13
-        dec qword[fib_arq]
-        cmp qword[fib_arq], 1
-        jne fibonacci
-        jmp arquivo
+        mov r13, r15 ; r13 = r15
+        add r15, r14 ; r15 = r15 + r14
+        mov [fib], r15 ; fib = r15
+        mov r14, r13 ; r14 = r13
+        dec qword[fib_arq] ; fib_arq -= 1
+        cmp qword[fib_arq], 1 ; fib_arq = 1?
+        jne fibonacci ; retornar para o começo da função fibonacci caso não seja 1
+        jmp arquivo ; caso seja 1, ir para função arquivo
 
 fib_1:
     mov qword[fib], 1
